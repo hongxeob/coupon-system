62c14afe210acd2df91646c9fa7da81398c65ba1
Hongseob Lee <sjun2918@naver.com>
2024-02-14 14:58:57 +0900

    update: Producer 적용

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -2,7 +2,7 @@ package com.example.api.service;
 
 import org.springframework.stereotype.Service;
 
-import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.respoistory.CouponCountRepository;
 import com.example.api.respoistory.CouponRepository;
 
@@ -11,10 +11,12 @@ public class ApplyService {
 
 	private final CouponRepository couponRepository;
 	private final CouponCountRepository couponCountRepository;
+	private final CouponCreateProducer couponCreateProducer;
 
-	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
 		this.couponRepository = couponRepository;
 		this.couponCountRepository = couponCountRepository;
+		this.couponCreateProducer = couponCreateProducer;
 	}
 
 	public void apply(Long userId) {
@@ -24,7 +26,7 @@ public class ApplyService {
 		if (count > 100) {
 			return;
 		}
-
-		couponRepository.save(new Coupon(userId));
+		//프로듀서 작업
+		couponCreateProducer.create(userId);
 	}
 }
